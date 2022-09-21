8cc693968a076ed6355e8686a7bac0ed4685f447
admin <admin@DESKTOP-TD0JBJ1>
2022-09-21 22:27:11 +0300

    Добавьте файлы проекта.

---
+++ WorkFIO/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkFIO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User user;
+       private ObservableCollection<User> _users;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _users = new ObservableCollection<User>();
+        }
+
+        private void ButtonRun_Click (object sender, RoutedEventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string middleName = middleNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+
+            user = new User(firstName, middleName, lastName);
+
+            _users.Add(user);
+
+            listView.ItemsSource = _users;
+            listView.DisplayMemberPath = "Fio";
+        }
+
+    }
+}
---
+++ WorkFIO/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFIO
+{
+    internal class User
+    {
+        public string Fio { get; set; }
+
+        public User(string firstName, string middleName, string lastName)
+        {
+            Fio = lastName + " " + firstName[0] + "." + middleName[0] + ".";
+        }
+    }
+}
